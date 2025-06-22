12599e715b6e7d2d3737ae57f2dcaf10986896af
annas-coder <mohamedannas1999@gmail.com>
2025-06-22 22:31:27 +0400

    Added new changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import { authService } from "../services/authService.js";
-import { ResponseJsonHandler } from "../utilizes/responseJsonHandler.js";
+import { ResponseJsonHandler } from "../helpers/responseJsonHandler.js";
 
 export class authController{
   static async createUser(req, res, next) {
