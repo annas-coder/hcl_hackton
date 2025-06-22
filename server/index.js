275832f2991e6833f367687fbf4569f87ceb3ed5
annas-coder <mohamedannas1999@gmail.com>
2025-06-22 22:33:44 +0400

    Added new change

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import { config } from "dotenv";
 import { db_connection } from "./config/db.js";
 import authRoute from "./routes/authRoutes.js"
